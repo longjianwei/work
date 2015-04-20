7fba7b1ad0da77a7edcd056452d2ad167fbed77b
longlijian <longlijian@gmail.com>
2015-04-20 20:19:33 +0800

    post

---
+++ basicTest/post.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+    <title>添加联系人</title>
+    </head>
+    <body>
+    <?php 
+        echo "<h1>add message to post_add.php<h2><br>";
+    ?>
+    <table align = "center" bgcolor ="#ccc" width ="500" border = "1">     
+    <form action = "post_add.php" method = "post">
+<div>
+    <tr><td> 编号：</td><td><input type = "text" name = "id" size="auto"></td></tr>
+    <tr><td> 姓名：</td><td><input type = "text" name = "name"></td></tr>
+    <tr><td> 公司：</td><td><input type = "text" name = "company"></td></tr>
+    <tr><td> 地址：</td><td><input type = "text" name = "adres"></td></tr><br>
+    <tr><td> 电话：</td><td><input type = "text" name = "phone"></td></tr><br>
+    <tr><td>  EMAIL：</td><td><input type= "text" neme = "email"></td></tr><br>
+    <tr><td><input type = "submit" value = "添加联系人"></td></tr>
+</div>
+     </form>
+    </table>
+    </body>
+</html>
